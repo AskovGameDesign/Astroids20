8e20805b713afe066876a2be61fcf6c1fa57c285
Claus Bjerre Jacobsen <claus@aneks.dk>
2020-12-01 12:23:53 +0100

    Project added

---
+++ Astroids/Assets/MyGame/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody2D rb2d;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Destroy(this.gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.AddRelativeForce(Vector2.up * speed, ForceMode2D.Impulse);
+    }
+}
---
+++ Astroids/Assets/MyGame/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float speed = 5f;
+    public float torqueForce = 2f;
+
+
+    private float forceInput;
+    private float torqueInput;
+   
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        forceInput = Input.GetAxis("Vertical");
+        torqueInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.AddForce(transform.up * forceInput * speed);
+        rb2d.AddTorque(-torqueInput * torqueForce);
+    }
+}
